d8d98f03fe0032bec1902835cd117ae98da2c1b6
wtmsdrjt-2nd <141470842+wtmsdrjt-2nd@users.noreply.github.com>
2023-11-25 05:53:49 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Watim Sudrajat";
+            $("#favicon").attr("href", "assets/images/cmsoon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
