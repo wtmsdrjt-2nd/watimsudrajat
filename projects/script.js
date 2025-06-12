1d324970056b607089cfcf54e8827d533e63686d
wtmsdrjt-2nd <141470842+wtmsdrjt-2nd@users.noreply.github.com>
2025-06-12 23:56:14 +0700

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -105,7 +105,7 @@ getProjects().then(data => {
 })
 // fetch projects end
 
-//--Start of Tawk.to Script--//
+// Start of Tawk.to Script
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
@@ -117,7 +117,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-//--End of Tawk.to Script--//
+// End of Tawk.to Script
 
 // disable developer mode
 document.onkeydown = function (e) {
