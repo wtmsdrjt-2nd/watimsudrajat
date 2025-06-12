63670989a6d0f6b7fa9d215d812cce795c8a3d5a
wtmsdrjt-2nd <141470842+wtmsdrjt-2nd@users.noreply.github.com>
2025-06-12 23:55:31 +0700

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -105,17 +105,19 @@ getProjects().then(data => {
 })
 // fetch projects end
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://tawk.to/chat/65c6d7c70ff6374032cb74db/1hm8blpp5';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+//--Start of Tawk.to Script--//
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65c6d7c70ff6374032cb74db/1hm8blpp5';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
 })();
-// End of Tawk.to Live Chat
+</script>
+//--End of Tawk.to Script--//
 
 // disable developer mode
 document.onkeydown = function (e) {
